5efe17297a2be47d62d65f0584a4c86ad3d1ae5f
mjlslink <mjlslink5@gmail.com>
2024-10-19 14:33:47 -0600

    switched log package

--- src/main/java/com/example/rqchallenge/employees/EmployeeService.java
+++ src/main/java/com/example/rqchallenge/employees/EmployeeService.java
@@ -4,18 +4,19 @@ import com.example.rqchallenge.employees.model.Employee;
 import com.example.rqchallenge.employees.model.EmployeeEntity;
 import com.example.rqchallenge.employees.model.Mapper;
 import com.sun.istack.NotNull;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 @Service
 public class EmployeeService {
 
-    private final Logger logger = Logger.getLogger(getClass().getName());
+    private static final Logger logger = LogManager.getLogger();
 
     private final EmployeeRepository repository;
     private final Mapper mapper;
@@ -33,6 +34,8 @@ public class EmployeeService {
 
     public List<Employee> getEmployees() {
 
+        logger.info("Getting all employees" );
+
         return repository.findAll()
                 .stream()
                 .map(mapper::toDTO)
@@ -47,7 +50,7 @@ public class EmployeeService {
                 .max()
                 .orElseThrow();
 
-        logger.info("Max salary is " + maxSalary);
+        logger.info("Max salary is {}", maxSalary);
 
         return employees.stream()
                 .filter(e -> e.getEmployeeSalary() < maxSalary)
@@ -62,6 +65,8 @@ public class EmployeeService {
 
     public List<Employee> getEmployeesByName(@NotNull String name){
 
+        logger.info("Getting employee information for {}", name);
+
         return repository.findByName(name).stream()
                 .filter(employee -> employee.getName().contains(name))
                 .map(mapper::toDTO)
@@ -72,6 +77,8 @@ public class EmployeeService {
     public Integer getHighestSalaryOfEmployees(){
         List<Employee> employees = getEmployees();
 
+        logger.info("Getting highest salary of all employees");
+
         return employees.stream()
                 .mapToInt(Employee::getEmployeeSalary)
                 .max()
@@ -79,11 +86,10 @@ public class EmployeeService {
     }
 
     public String deleteEmployeeById(String id) {
-        System.out.println("Deleting ");
         EmployeeEntity byId = repository.getById(Integer.parseInt(id));
         String name = byId.getName();
 
-        logger.info("Deleting " + name);
+        logger.info("Deleting {}", name);
 
         repository.delete(byId);
 
